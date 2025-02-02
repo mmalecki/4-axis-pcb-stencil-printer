6f2782479cbcdd411cd62f17979031b170a5318c
Maciej Małecki <me@mmalecki.com>
2025-02-02 04:57:33 +0100

    Add new Z axis parts
    Tidy up `settings.py`.

--- cad/settings.py
+++ cad/settings.py
@@ -1,3 +1,5 @@
+import cq_queryabolt as queryabolt
+
 class Settings:
     # This is an inherited project, so lots of dimensions will
     # be put in as-is.
@@ -10,4 +12,30 @@ class Settings:
     basePlateWidth = 162
     basePlateThickness = 2
 
-    boltR = 1.6
+    # Fits and design constraints
+    pressFit = 0.05
+    tightFit = 0.1
+    fit = 0.2
+    looseFit = 0.5
+    wallThickness = 4
+
+    # Bolts
+    bolt = "M3"
+    frameBolt = "M4"
+    boltFit = fit
+
+    # Bolt helpers
+    boltR = (queryabolt.boltData(bolt)["diameter"] + boltFit) / 2
+    frameBoltR = (queryabolt.boltData(frameBolt)["diameter"] + boltFit) / 2
+    frameBoltHeadD = queryabolt.boltData(frameBolt, kind="socket_head")["head_diameter"] + looseFit
+
+    # Motion system
+    rodD = 8
+
+    bearingD = 15
+    bearingFit = tightFit
+    bearingH = 24
+    extD = 20
+
+    # Others parameters
+    compliance = 1
---
+++ cad/z-axis.py
@@ -0,0 +1,115 @@
+import math
+import cadquery as cq
+from settings import Settings
+from workplane import Workplane
+
+fit, bearingFit = Settings.fit, Settings.bearingFit
+wallT, rodD = Settings.wallThickness, Settings.rodD
+extD, bearingD = Settings.extD, Settings.bearingD
+bearingH = Settings.bearingH
+
+bearingOffY = (bearingD + bearingFit + wallT) / 2
+
+# Technically this only needs:
+# frame bolt head | wall | bearing | wall | frame bolt head
+# But we're throwing in a loose wall there for aesthetic reasons,
+# otherwise the mount sticks out of the aluminium extrusion.
+bearingMountW = bearingD + wallT * 3 + 2 * Settings.frameBoltHeadD 
+
+def rodMount():
+    bD = bearingD + bearingFit
+
+    rD = rodD + fit
+    rW = rD / 2+ wallT
+
+    w = bearingMountW
+    bearingWallT = (bD - rD) / 2 + wallT
+
+    rm = (Workplane("XY")
+          .sketch()
+          .circle(rW)
+          .push([(0, bearingOffY- (bearingWallT - wallT) / 2)])
+          .rect(w, bearingWallT)
+          .push([(w / 4, rD / 4)])
+          .rect(w / 2, rD)
+          .reset()
+          .circle(rD / 2, mode='s')
+          .push([(w / 4, rD / 4)])
+          .rect(w / 2, Settings.compliance, mode='s')
+          .reset()
+          .finalize()
+          .extrude(extD + wallT)
+    )
+    rm = (rm.faces(">Y[3]").workplane(centerOption="CenterOfBoundBox")
+          .move(0, wallT / 2)
+          .cboreBoltHole(Settings.frameBolt, clearance=Settings.boltFit)
+          )
+    rm = (rm.faces(">Y[0]").workplane(centerOption="CenterOfBoundBox")
+          .move(0, wallT / 2)
+          .cboreBoltHole(Settings.frameBolt, clearance=Settings.boltFit, cboreDepth = 0.2)
+          )
+    return (rm, w, bearingWallT)
+
+def bracket():
+    w = extD * 3
+    br = (Workplane("XY")
+          .sketch()
+          .rect(extD, w)
+          .push([(w / 2 - extD / 2, -w / 2 + extD / 2)])
+          .rect(w, extD)
+          .reset()
+          .push([(extD, 0)])
+          .rarray(extD, extD, 3, 3)
+          .circle(Settings.frameBoltR, mode='s')
+          .finalize()
+          .extrude(wallT)
+          )
+
+    (rm, rmW, rmT) = rodMount()
+
+    br = br + rm.translate((rmW / 2 - extD / 2, -extD * 1.5 - (bearingD + bearingFit + 2 * wallT) / 2 ))
+
+    brace = (cq.Workplane("XY")
+            .rect(wallT / 2, 2 * rmT)
+            .extrude(rmT * 2).translate((-extD / 2 - wallT / 4, -extD * 1.5))
+            .edges(">Y and >Z").chamfer(rmT)
+             )
+    br = br + brace
+
+    br = (br.faces(">>Z[1]").edges("|Z").chamfer(wallT / 4))
+    br = br.faces("<Z or >Z").edges("not %Circle").chamfer(wallT / 8)
+    br = (br.faces(">Z[2]").edges(">X").chamfer(wallT / 10))
+    br = (br.faces("<X").chamfer(wallT / 8))
+
+    return br
+
+def bearingMount():
+    bD = bearingD + bearingFit
+
+    rm = (Workplane("XY")
+          .sketch()
+          .circle(bD / 2 + wallT)
+          .circle(bD / 2, mode='s')
+          .push([(0, bearingOffY)])
+          .rect(bearingMountW + fit, wallT)
+          .finalize()
+          .extrude(bearingH)
+    )
+    extOff = (bearingH - extD) / 2
+    rm = (rm.faces(">Y[0]").workplane(centerOption="CenterOfBoundBox")
+          .center(0, -extOff)
+          .rarray((bearingMountW - Settings.frameBoltHeadD - wallT / 2), 1, 2, 1)
+          .cboreBoltHole(Settings.frameBolt, clearance=Settings.boltFit, cboreDepth=0.2)
+          )
+
+    rm = rm.edges(">Z and |Y and (>X or <X)").chamfer(wallT * 2)
+    rm = (rm.edges("|Z").edges("<<Y[1] and (not (>>X or <<X))").fillet(wallT / 2))
+    rm = rm.edges(">Z or <Z or (>>X and >>Y) or (<<X and >>Y)").chamfer(wallT / 8)
+
+    return (rm, bearingMountW)
+
+br = bracket()
+show_object(br)
+
+(bm, _) = bearingMount()
+show_object(bm.translate((0, 0, bearingH)), name="bearingMount")
