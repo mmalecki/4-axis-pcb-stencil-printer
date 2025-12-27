df9a501c532c846f4e765618471d21ebaa0c5286
Maciej Małecki <me@mmalecki.com>
2025-12-27 12:23:31 +0100

    Use `ocp_vscode` in `top-plate.py`

--- cad/top-plate.py
+++ cad/top-plate.py
@@ -1,19 +1,17 @@
 import math
 import cadquery as cq
 from settings import Settings
+from ocp_vscode import *
 
 width = Settings.topPlateWidth
 length = Settings.topPlateLength
 thickness = Settings.topPlateThickness
 
 boltR = Settings.boltR
-rotateBoltS = 75.450
+rotateBoltS = Settings.rotateBoltS
 boltS = 30
 
 def topPlate():
-    lengthBolts = math.floor(length / boltS)
-    widthBolts = math.floor(width / boltS)
-
     sketch = (cq.Sketch()
         .rect(length, width)
         .parray(rotateBoltS, 0, 360, 5)
@@ -22,6 +20,8 @@ def topPlate():
     )
 
     if Settings.topPlateHolePattern:
+        lengthBolts = math.floor(length / boltS)
+        widthBolts = math.floor(width / boltS)
         sketch = (sketch
             .rarray(boltS, boltS, lengthBolts, widthBolts).circle(boltR, mode='s')
             .reset()
@@ -35,5 +35,6 @@ plate = topPlate()
 plate.faces(">Z").workplane().section().export("output/top-plate-z.dxf")
 plate.faces(">Y").workplane().section().export("output/top-plate-y.dxf")
 
-if 'show_object' in globals():
-    show_object(topPlate())
+if __name__ == '__main__':
+    top_plate = topPlate()
+    show(top_plate)
